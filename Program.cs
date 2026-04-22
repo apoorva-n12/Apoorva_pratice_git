c5a12057d5d5c08c83fe94ac2de3b148c87fbe5c
Apoorva <apoorva.n@intimetec.com>
2026-04-22 19:44:04 +0530

    A

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyPracticePrograms
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine() ?? "0");
+
+            int fact = 1;
+
+            for (int i = 1; i <= num; i++)
+            {
+                fact = fact * i;
+            }
+
+            Console.WriteLine("Factorial is: " + fact);
+        }
+    }
+}
